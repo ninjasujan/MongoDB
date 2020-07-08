be7a1c799e8d66b5143caf1b6a0c2ccf7844cbe6
sujan <sujanpoojary884@gmail.com>
2020-07-08 18:06:29 +0530

    and or nor equality check done

---
+++ module-4/command.js
@@ -0,0 +1,29 @@
+// comparision operator
+db.movies.find({ runtime: { $lt: 60 } }).pretty();
+db.movies.find({ runtime: { $lte: 60 } }).pretty();
+db.movies.find({ runtime: { $ne: 60 } }).pretty();
+db.movies.find({ runtime: { $eq: 60 } }).pretty();
+
+/* Querying embedded doc */
+db.movies.find({ "rating.average": { $gt: 7 } });
+// in array
+db.movies.find({ genres: "Drama" }); // there is item Drama in the genres array
+db.movies.find({ genres: ["Drama"] }); // looking for an array exact ["Drama"] array in generes field
+
+// continue - comparisions
+db.movies.find({ runtime: { $in: [30, 42] } }); // look for the document runtime is 30 or 40
+db.movies.find({ runtime: { $nin: [30, 42] } });
+
+// $or and $nor condition
+db.movies.find({
+  $or: [{ "rating.average": { $lt: 5 } }, { "rating.average": { $gt: 9.3 } }],
+});
+
+db.movies.find({
+  $nor: [{ "rating.average": { $lt: 5 } }, { "rating.average": { $gt: 9.3 } }],
+});
+
+// $and
+db.movies.find({
+  $and: [{ "rating.average": { $gt: 9 } }, { genres: "Drama" }],
+});
