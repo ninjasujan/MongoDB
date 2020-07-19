c7f95972cc7f66abd7e7ec7bba8030219f3abd0b
sujan <sujanpoojary884@gmail.com>
2020-07-19 18:13:12 +0530

    array elements update done

--- module-5/command.js
+++ module-5/command.js
@@ -50,3 +50,81 @@ db.users.updateOne(
   { $set: { age: 29, hobbies: [{ title: "cooking", frequency: 5 }] } },
   { upsert: true }
 );
+
+// Assignments
+// use sports
+db.soprts.insertMany([
+  { title: "Creed", requiredTeam: false },
+  { title: "Johnson", requiredTeam: true },
+  { title: "Trivago", requiredTeam: false },
+]);
+
+db.soprts.updateOne(
+  { title: "AJ" },
+  { $set: { requiredTeam: true } },
+  { upsert: true }
+);
+
+db.soprts.updateMany({ requiredTeam: true }, { $set: { playerRequired: 10 } });
+
+db.soprts.updateMany({ requiredTeam: true }, { $inc: { playerRequired: 10 } });
+
+/* Update on Array */
+// Updating matched array element
+db.users.updateMany(
+  {
+    hobbies: { $elemMatch: { title: "Cooking", frequency: { $gte: 6 } } },
+  },
+  { $set: { "hobbies.$.highFreq": true } }
+);
+
+// Updating All Array element
+db.users.updateMany(
+  { "hobbies.frequency": { $gte: 3 } },
+  { $set: { "hobbies.$.goodFreq": true } }
+);
+
+// The above command updates only first doc of the matched array elements
+db.users.updateMany(
+  { age: { $gt: 30 } },
+  { $inc: { "hobbies.$[].frequency": -2 } }
+);
+
+db.users.updateOne({ name: "Chris" }, { $set: { marks: [45, 55, 65] } });
+
+db.users.updateOne({ name: "Chris" }, { $inc: { "marks.$[]": 1 } });
+
+db.users.updateMany(
+  { age: { $gte: 20 } },
+  { $set: { "hobbies.$[el].modified": true } },
+  { arrayFilters: [{ "el.goodFreq": true }] }
+);
+
+db.users.updateOne(
+  { name: "Chris" },
+  { $push: { title: "Swimming", frequency: 6 } }
+);
+
+db.users.updateMany(
+  { name: "Chris" },
+  {
+    $push: {
+      hobbies: {
+        $each: [
+          { title: "Running", frequency: 6 },
+          { title: "Boxing", frequency: 8 },
+        ],
+        $sort: { frequency: -1 },
+      },
+    },
+  }
+);
+
+// $pull element basd on condition
+db.users.updateOne(
+  { name: "Chris" },
+  { $pull: { hobbies: { title: "Boxing" } } }
+);
+
+//$pull element from last
+db.users.updateOne({ name: "Chris" }, { $pop: { hobbies: 1 } });
