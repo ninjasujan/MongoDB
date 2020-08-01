f8c1c8e42ef20168bc168b2c5cbec2acc1145be0
sujan <sujanpoojary884@gmail.com>
2020-08-01 11:07:55 +0530

    partialFilter expression

--- module-6/index.js
+++ module-6/index.js
@@ -10,3 +10,42 @@ db.contacts.createIndex({ "dob.age": 1, gender: 1 });
 
 /*  Index is overhead incase we are searching full portion of a document and when no record match the search criteria 
 (in this case records already in memory so no need to create the index) */
+
+// Sorting in Index
+db.contacts
+  .explain("executionStats")
+  .find({ "dob.age": { $gt: 20 } })
+  .sort({ age: 1 });
+
+/* Drop Index */
+db.contacts.dropIndex();
+
+/* Index configuration - Unique Index */
+db.contacts.createIndex({ email: 1 }, { unique: true });
+// helps to maintain uniquness in the collection, in which filed the index is upon created
+
+/* List all created index on collection */
+db.contacts.getIndexes();
+
+/* PartialFilterExpression */
+db.contacts.createIndex(
+  { "dob.age": 1 },
+  { partialFilterExpression: { gender: "male" } }
+);
+
+// while fetching the records if the gender female appeares then it will go for coloumn scan
+
+/* Apply partial indexes */
+db.contacts.insertMany([
+  { name: "AAA", email: "aa@gmail.com" },
+  { name: "BBB", email: "bb@gmail.com" },
+]);
+db.contacts.createIndex({ email: 1 }, { unique: true });
+db.contacts.insertOne({ name: "DDD" });
+db.contacts.insertOne({ name: "CCC" }); // throws error
+// MongoDB gives error if 2 doc has no value for index field and if that field is decalred as unique
+// to avoid this use partialFilterExpression
+db.contacts.createIndex(
+  { email: 1 },
+  { unique: true, partialFilterExpression: { $email: { $exist: true } } }
+);
